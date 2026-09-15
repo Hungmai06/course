08a2c2d5cf1a8dd8e1ee7635d3b70a975caf9797
unknown <maivanhung0604@gmail.com>
2026-09-16 00:57:03 +0700

    first

--- backend/src/main/java/com/example/khoahocdrive/service/Impl/CourseServiceImpl.java
+++ backend/src/main/java/com/example/khoahocdrive/service/Impl/CourseServiceImpl.java
@@ -33,6 +33,7 @@ import com.example.khoahocdrive.repository.FullCourseConfigRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
